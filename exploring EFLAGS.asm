6c95aa87908c4c294833585de2b480ba2f78e314
Sneferu777 <haithamderrazi@gmail.com>
2025-07-26 19:04:59 +0100

    Update exploring EFLAGS.asm

--- exploring EFLAGS.asm
+++ exploring EFLAGS.asm
@@ -5,6 +5,6 @@ global _start
 _start:
         mov al,0b11111111 ; 0b means start of a binary
         mov bl,0b0001 ; taking only 8 bits = 1bytes from each reg
-        add al, bl ; THIS WILL RESULT A SEGMENTATION FAULT
+        add al, bl ; al will be zero and CF(carry flag) = 1
         adc ah,0        ;it ll add 0 to ah and the Carry as well
-        int 0x80
+        
