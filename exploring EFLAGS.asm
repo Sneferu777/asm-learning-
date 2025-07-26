5c0506f8cbb92c6900195cf2914243f2335a316e
Sneferu777 <haithamderrazi@gmail.com>
2025-07-26 18:52:07 +0100

    Create exploring EFLAGS.asm

---
+++ exploring EFLAGS.asm
@@ -0,0 +1,10 @@
+section .data
+
+section .text
+global _start
+_start:
+        mov al,0b11111111 ; 0b means start of a binary
+        mov bl,0b0001 ; taking only 8 bits = 1bytes from each reg
+        add al, bl ; THIS WILL RESULT A SEGMENTATION FAULT
+        adc ah,0        ;it ll add 0 to ah and the Carry as well
+        int 0x80
