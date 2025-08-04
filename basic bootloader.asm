617d9419f305fa95d631f264c7419b67a97c4c65
Sneferu777 <haithamderrazi@gmail.com>
2025-08-04 14:52:41 +0100

    Update basic bootloader.asm

--- basic bootloader.asm
+++ basic bootloader.asm
@@ -1,32 +1,50 @@
-; boot.asm
-BITS 16                 ; We're working in 16-bit real mode
-ORG 0x7C00              ; BIOS loads boot sector to this address
+ORG 0x7c00      ; origin tells NASM at what mem addr your code will be loaded and executed
+                ; we use 0x7c00 , when a pc boots form a disk the BIOS loads the first 512 bytes (the boot sector) into memory
+                ; at address 0x7c00, then jumps to it 
+BITS 16
 
-start:
-    mov si, msg         ; Load address of string into SI
-    call print_string   ; Call function to print string
 
-hang:
-    hlt                 ; Halt CPU to save power
-    jmp hang            ; Infinite loop
+main:
+        mov ax,0
+        mov ds, ax
+        mov es, ax
+        mov ss, ax
 
-; === Function: print_string ===
-; Prints null-terminated string pointed to by SI using BIOS interrupt
-print_string:
-    mov ah, 0x0E        ; Teletype mode (INT 10h AH=0x0E)
-.next_char:
-    lodsb               ; Load byte at DS:SI into AL, advance SI
-    or al, al           ; Check for null terminator
-    jz .done
-    int 0x10            ; BIOS call to print AL to screen
-    jmp .next_char
-.done:
-    ret
+        mov sp, 0x7c00
+        mov si, os_boot_msg
+        call print
+        hlt             ; halts the CPU  until next hardware interrupt, after bootloader is done, nothing else is loaded so the CPU would
+                        ; continue executing garbage
 
-msg db "Hello, Sneferu's Bootloader!", 0  ; Null-terminated string
+halt:
+        jmp halt        ;loops forever to prevent system crash
 
-; Fill remaining bytes to make total size = 510
-times 510 - ($ - $$) db 0
+print:
+        push si
+        push ax
+        push bx
 
-; Boot signature (must be 0x55AA at the end)
-dw 0xAA55
+print_loop:
+        LODSB           ; load string byte , load a bytes from [ds:si] into al then increment si(or decrement if DF is set
+                        ; it s a clen way to loop through a string one character at a time
+        or al,al
+        jz done_print
+
+        mov ah, 0x0e    ; teletype output
+        mov bh, 0       ;page number usually 0
+        int 0x10 ; video interrupt
+
+        jmp print_loop
+
+done_print:
+        pop ax
+        pop bx
+        pop si
+        ret
+
+
+os_boot_msg: db 'Our os has loaded!',0xA,0
+
+TIMES 510-($-$$) DB 0   ; filling the first 510 bytes with 0
+DW 0AA55H               ; filling the last word or 2 bytes (the Magic Bytes) with 0xAA55H , means write the boot signature at byte 511-512
+                        ; wichi tells " yes i m a bootloader"
